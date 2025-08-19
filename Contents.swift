8d50afbe9330b1757596076659c9f2355fc8566f
Jim Mittler <jamesmittlerii@my.smccd.edu>
2025-08-19 16:47:23 -0700

    latest

--- Contents.swift
+++ Contents.swift
@@ -15,7 +15,7 @@
 import Cocoa
 
 
-let names = ["Anna": 18 , "Alex": 19, "Brian": 20, "Jack": 21, "Jill": 24]
+let names = ["Anna": 17 , "Alex": 19, "Brian": 31, "Jack": 21, "Jill": 66]
 
 let greetings = ["Good morning", "Good day", "Good afternoon", "Good evening", "Good night"]
 
@@ -23,4 +23,20 @@ for (name, age) in names {
     
     let randomInt = Int.random(in: 0..<5)
     print("\(greetings[randomInt]), \(name)! Happy \(age)th Birthday!")
+    
+    switch age {
+    case Int.min..<18:
+        print("You're a child!")
+
+    case 18...25:
+        print("You're a young adult")
+
+    case 26...65:
+        print("You're an adult!")
+    case 66...Int.max:
+        print("You're a senior!")
+
+    default:
+        print("no clue!?")
+    }
 }
